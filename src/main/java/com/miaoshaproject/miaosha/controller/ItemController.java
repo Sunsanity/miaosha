1fa06186bb8d31a9fc943fe82f23a20100e66054
sunjingwen <sunjw@think-land.com>
2020-07-28 17:47:23 +0800

    商品模块

--- src/main/java/com/miaoshaproject/miaosha/controller/ItemController.java
+++ src/main/java/com/miaoshaproject/miaosha/controller/ItemController.java
@@ -1,24 +1,19 @@
 package com.miaoshaproject.miaosha.controller;
-
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.miaoshaproject.miaosha.db.Item;
 import com.miaoshaproject.miaosha.db.Stock;
-import com.miaoshaproject.miaosha.db.User;
-import com.miaoshaproject.miaosha.error.BusinessException;
-import com.miaoshaproject.miaosha.error.EmBusinessError;
 import com.miaoshaproject.miaosha.response.CommonReturnType;
 import com.miaoshaproject.miaosha.service.IItemService;
 import com.miaoshaproject.miaosha.service.IStockService;
 import com.miaoshaproject.miaosha.vo.ItemVO;
-import com.miaoshaproject.miaosha.vo.UserVO;
+import com.sun.istack.internal.NotNull;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-
 import java.math.BigDecimal;
-import java.util.Objects;
-
-import static com.miaoshaproject.miaosha.controller.BaseController.CONTENT_TYPE_FORMED;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -30,8 +25,9 @@ import static com.miaoshaproject.miaosha.controller.BaseController.CONTENT_TYPE_
  */
 @RestController
 @RequestMapping("/item")
+@Validated
 @CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
-public class ItemController {
+public class ItemController extends BaseController{
 
     @Autowired
     private IItemService itemService;
@@ -46,7 +42,7 @@ public class ItemController {
                                        @RequestParam("description") String description,
                                        @RequestParam("price") BigDecimal price,
                                        @RequestParam("stock") Integer stock,
-                                       @RequestParam("imgUrl") String imgUrl) throws BusinessException {
+                                       @RequestParam("imgUrl") String imgUrl) {
         Item item = new Item();
         item.setTitle(title);
         item.setDescription(description);
@@ -63,6 +59,34 @@ public class ItemController {
         BeanUtils.copyProperties(item, itemVO);
         itemVO.setStock(stock1.getStock());
         return CommonReturnType.create(itemVO);
+    }
+
+    /**
+     * 商品详情查询
+     */
+    @GetMapping(value = "/get")
+    public CommonReturnType getItem(@NotNull Integer id) {
+        Item item = itemService.getById(id);
+        Stock stock = stockService.getOne(new QueryWrapper<Stock>().eq("item_id", id));
+        ItemVO itemVO = new ItemVO();
+        BeanUtils.copyProperties(item, itemVO);
+        itemVO.setStock(stock.getStock());
+        return CommonReturnType.create(itemVO);
+    }
+
+    /**
+     * 商品列表
+     */
+    @GetMapping(value = "/list")
+    public CommonReturnType listItem() {
+        List<Item> itemList = itemService.list();
+        List<ItemVO> itemVOS = itemList.stream().map(o -> {
+            ItemVO itemVO = new ItemVO();
+            BeanUtils.copyProperties(o, itemVO);
+            itemVO.setStock(stockService.getOne(new QueryWrapper<Stock>().eq("item_id", o.getId())).getStock());
+            return itemVO;
+        }).collect(Collectors.toList());
+        return CommonReturnType.create(itemVOS);
     }
 
 }
